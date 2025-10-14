4011b7bcb07c5abadd6143c25a92f693e5e7e278
AmirHossein AliMohammadi <ahamdev.programmer@gmail.com>
2025-10-14 18:16:43 +0330

    clean utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -2,18 +2,29 @@
 from dotenv import load_dotenv
 from pydantic import BaseSettings
 
+# Load environment variables from .env
 load_dotenv()
 
+
 class Settings(BaseSettings):
+    # Telegram
     BOT_TOKEN: str
     ADMINS: str
+
+    # Marzban API
     MARZBAN_HOST: str
+
+    # Marzban Accounts
     MARZBAN_USER_VIP: str
     MARZBAN_PASS_VIP: str
+
     MARZBAN_USER_TEST: str
     MARZBAN_PASS_TEST: str
+
     MARZBAN_USER_FREE: str
     MARZBAN_PASS_FREE: str
+
+    # Database Configuration
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
@@ -23,5 +34,6 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+
 
 settings = Settings()
